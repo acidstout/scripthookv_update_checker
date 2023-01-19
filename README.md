e92ea54cd100e08444a05b487c91d2600b5c7ce6
acid <acid@WEBDEVHEAD>
2023-01-19 18:43:35 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# scripthookv_update_checker
-Grand Theft Auto V ScriptHookV Update Checker
+#Grand Theft Auto V ScriptHookV Update Checker
+Checks the installed version of the ScriptHookV plugin against the latest version online, and updates it if necessary.
+
+##Features
+- Gets game folder from registry.
+- Fetches local versions directly from files.
+- Deletes old plugin if GTA has been updated, but the plugin has not yet been updated in order to not break GTA.
+- Logs to file by default.
+
+##Disclaimer
+This script could have been cleaner and nicer from a code perspective, but I wanted a fast, reliable, portable single-file solution without many dependencies. So, the script tries to do as much as possible itself. You should not need to modify it in order to make it work for you, but keep in mind that although it works totally fine for me, it might not work for you at all, or it might even destroy something. So, use this on your own risk.
+
+##Dependencies
+PowerShell 7. At least it has been tested with that version. It does not run with PowerShell 1.0, which you shouldn't use anyway. It might run with an PowerShell lower than version 7.0, but that hasn't been tested.
+
+##Usage
+1. Run the script.
+2. You may want to create a cronjob.
+
+##Cronjob
+Use Windows' Task Scheduler and create a new task running once per day (preferably at night), and let it run this command:
+
+```
+pwsh scripthookv_update_checker.ps1
+```
+
+You will likely need to add the proper paths to the command, but basically it's just that. Save the task and you're good to go. Please don't hammer Alexander's website by constantly checking for a new version. Once per day is more than enough.
---
+++ scripthookv_update_checker.ps1
@@ -0,0 +1,145 @@
+#
+# ScriptHookV Update Checker for Grand Theft Auto V
+#
+# Successfully tested on PowerShell 7.
+# Does NOT work on PowerShell 1.0.
+#
+# @author: nrekow
+# @version: 1.0
+#
+
+# Disable those red error messages in case of errors, because we use Try & Catch everywhere.
+$ErrorActionPreference = "Stop"
+
+# If TRUE no output will be printed. Instead errors will be logged into a file.
+$QuietMode = $true
+
+# Define fallback version for cases where ScriptHookV is not installed.
+$Fallback_ScriptHookV_Version = '0.0'
+
+# Get location and name of current script.
+# Used to create a logfile of the same name.
+$scriptName = (Get-Item $PSCommandPath).Basename
+$scriptLog = "$PSScriptRoot\$scriptName.log"
+
+
+# Either write into the console or into the log file,
+# depending on the $QuietMode setting.
+#
+# Param String $logstring
+#
+Function LogWrite {
+	Param ([string]$logstring)
+	If ($QuietMode -eq $false) {
+		Write-Output "$logstring`r`n"
+	} else {
+		$Timestamp = (Get-Date).ToString('yyyy-MM-dd hh:mm:ss')
+		Add-content $scriptLog -value ("[" + $Timestamp + "] " + $logstring)
+	}
+}
+
+
+# Get location from registry where GTA V is installed.
+Try {
+	$Game_Folder = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Rockstar Games\Grand Theft Auto V\' -Name 'InstallFolder'
+} Catch {
+	LogWrite "Grand Theft Auto V does not seem to be installed."
+	Exit
+}
+
+# Set ScriptHookV URLs.
+$ScriptHookV_URL = 'http://www.dev-c.com/gtav/scripthookv/'
+$ScriptHookV_Download_URL = 'http://www.dev-c.com/files/ScriptHookV_<VERSION>.zip'
+
+# Get installed ScriptHookV version.
+Try {
+	$ScriptHookV_Version = (Get-Item ($Game_Folder + '\ScriptHookV.dll')).VersionInfo.FileVersion
+} Catch {
+	# ScriptHookV plugin is not installed.
+	$ScriptHookV_Version = $Fallback_ScriptHookV_Version
+}
+
+# Get installed game version.
+Try {
+	$Game_Version = (Get-Item ($Game_Folder + '\GTA5.exe')).VersionInfo.FileVersion
+} Catch {
+	LogWrite "Could not find GTA5.exe in folder $Game_Folder."
+	Exit
+}
+
+# Get ScriptHookV version from website.
+Try {
+	$req = Invoke-WebRequest -Method GET -Uri $ScriptHookV_URL
+	$HTML = New-Object -Com "HTMLFile"
+	[string]$htmlBody = $req.Content
+	$HTML.write([ref]$htmlBody) # This is throws an error in PowerShell 1.0.
+	$filter = $HTML.getElementById('tfhover')
+	$ScriptHookV_Remote_Version = $filter.innerText.split("`r`n")[1].Replace('Versionv', '')
+} Catch {
+	LogWrite "Could not fetch latest ScriptHookV plugin version from website $ScriptHookV_URL."
+	Exit
+}
+
+# Show version found.
+If ($QuietMode -eq $false) {
+	Write-Output "`r`nScriptHookV Update Checker`r`n"
+	Write-Output "Installed game version: $Game_Version"
+	Write-Output "Installed plugin version: $ScriptHookV_Version"
+	Write-Output "Latest plugin version: $ScriptHookV_Remote_Version`r`n"
+}
+
+If ([System.Version]$ScriptHookV_Version -lt [System.Version]$Game_Version) {
+	# Installed ScriptHookV version is older than game version.
+	LogWrite "ScriptHookV is outdated."
+	
+	Try {
+		# Delete old ScriptHookV plugin
+		Remove-Item ($Game_Folder + '\ScriptHookV.dll') -Force
+	} Catch {
+		LogWrite "Could not delete outdated ScriptHookV plugin. You should remove it manually."
+		Exit
+	}
+	
+	If ([System.Version]$ScriptHookV_Remote_Version -ge [System.Version]$Game_Version) {
+		# Remote version is newer or equal than game version.
+		LogWrite "Downloading new version ..."
+		
+		$ScriptHookV_Download_URL =	$ScriptHookV_Download_URL.Replace('<VERSION>', $ScriptHookV_Remote_Version)
+		$Destination_File = ($Game_Folder + '\' + $(Split-Path -Path $ScriptHookV_Download_URL -Leaf))
+		
+		Try {
+			# Download new ScriptHookV zip-file.
+			Invoke-WebRequest $ScriptHookV_Download_URL -Headers @{"Referrer"=$ScriptHookV_URL} -OutFile $Destination_File
+		} Catch {
+			LogWrite "Could not download latest version from website $ScriptHookV_Download_URL."
+			Exit
+		}
+		
+		# Unzip the DLL from the downloaded file into the game's folder.
+		LogWrite "Unpacking new version ..."
+		
+		Try {
+			Add-Type -Assembly System.IO.Compression.FileSystem
+			$zip = [IO.Compression.ZipFile]::OpenRead($Destination_File)
+			$zip.Entries | where {$_.Name -like 'ScriptHookV.dll'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $Game_Folder, $true)}
+			$zip.Dispose()
+		} Catch {
+			LogWrite "Could not unpack ScriptHookV plugin into game folder."
+			Exit
+		}
+		
+		Try {
+			# Delete zip file after unpacking DLL.
+			Remove-Item $Destination_File -Force
+		} Catch {
+			LogWrite "Could not delete downloaded zip-file. You may need to remove it manually."
+		}
+		
+		LogWrite "Done!"
+	} Else {
+		# If the remote version is older than the game version, then there's no updated plugin, yet.
+		LogWrite "No update available, yet."
+	}
+} Else {
+	LogWrite "No update needed."
+}
